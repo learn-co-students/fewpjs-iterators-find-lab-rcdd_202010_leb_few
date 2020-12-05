39c9da49ced24ce897ed1bddffa6549e0d71ace4
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-05 08:47:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  let res = record.find(record => record.result === "W")
+     return !!res ? res.year : undefined
 }
