ab559b337fb58444f25cdec20109e20b7d8f8185
Jana Khanji <jana.khanji@ieee.org>
2020-12-08 10:36:19 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+if(result){return result.year}
+return undefined
+}
