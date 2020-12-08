978589cd5018e0a95b9255c89fe6204711dd98ee
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-08 08:00:51 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (record) => {
+  let win = record.find(x => x.result === "W")
+  return !!win? win.year : undefined
+}
