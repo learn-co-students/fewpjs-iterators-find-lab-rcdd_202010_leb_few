0993bff240ddd1e105adb31a0175922e46f7fb8e
Joseph Tarrazi <joseftaerrazi@gmail.com>
2020-12-13 08:49:45 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(superbowls){
+  let win = superbowls.find(superbowl => superbowl.result === "W")
+  return win ? win.year : undefined;
+}
